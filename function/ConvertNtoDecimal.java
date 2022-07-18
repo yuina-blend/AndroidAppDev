eb5c7e9e830cf1b5652472afe0e6f6d7daa1cb78
yuina-blend <moon.h14.dd2.as@gmail.com>
2022-07-18 21:12:29 +0900

    Create N進数変換機能

---
+++ function/ConvertNtoDecimal.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+public class Hello {
+    public String convert(String X, String N) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(N);
+        int A = Integer.parseInt(X, n);
+        return Integer.toString(A);
+    }
+
+    public static void main(String[] args) {
+        Hello a = new Hello();
+        System.out.println(a.convert("101101110", "2"));
+    }
+}
